b0ab867a616ccbf0fc0622bef0449bf6ccc8278d
Nibbl-z <thisisnotnibblz123@gmail.com>
2025-12-15 19:28:39 -0700

    feat(mainpage): :sparkles: add the brief description and nameplate and languages and tools and stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    let langs = [
+        ["https://raw.githubusercontent.com/vscode-icons/vscode-icons/c15969c0d34ac738bde5a6416fc9c79727eaee37/icons/file_type_luau.svg", "Luau"],
+        ["https://raw.githubusercontent.com/vscode-icons/vscode-icons/c15969c0d34ac738bde5a6416fc9c79727eaee37/icons/file_type_rust.svg", "Rust"],
+        ["https://raw.githubusercontent.com/vscode-icons/vscode-icons/c15969c0d34ac738bde5a6416fc9c79727eaee37/icons/file_type_lua.svg", "Lua"],
+        ["https://raw.githubusercontent.com/vscode-icons/vscode-icons/b07f49907d1da36efabaa5a18989cb66aef006d3/icons/file_type_go_gopher.svg", "Go"],
+        ["https://raw.githubusercontent.com/vscode-icons/vscode-icons/b07f49907d1da36efabaa5a18989cb66aef006d3/icons/file_type_svelte.svg", "Svelte"],
+        ["https://raw.githubusercontent.com/vscode-icons/vscode-icons/b07f49907d1da36efabaa5a18989cb66aef006d3/icons/file_type_typescript_official.svg", "Typescript"],
+        ["https://raw.githubusercontent.com/vscode-icons/vscode-icons/b07f49907d1da36efabaa5a18989cb66aef006d3/icons/file_type_c.svg", "C"],
+        ["https://raw.githubusercontent.com/vscode-icons/vscode-icons/b07f49907d1da36efabaa5a18989cb66aef006d3/icons/file_type_assembly.svg", "Assembly"],
+        ["https://raw.githubusercontent.com/vscode-icons/vscode-icons/b07f49907d1da36efabaa5a18989cb66aef006d3/icons/file_type_python.svg", "Python"],
+        ["https://raw.githubusercontent.com/vscode-icons/vscode-icons/b07f49907d1da36efabaa5a18989cb66aef006d3/icons/file_type_css.svg", "CSS (I hate.)"]
+    ]
+
+    let tools = [
+        ["https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg", "VS Code"],
+        ["https://love2d.org/favicon.ico", "Love2D"],
+        ["https://upload.wikimedia.org/wikipedia/commons/4/42/Roblox_Studio_Logo_2022.png", "Roblox Studio"],
+        ["https://elttob.uk/Fusion/0.3/assets/logo-dark.svg", "Fusion"],
+        ["https://avatars.githubusercontent.com/u/4350249?s=200&v=4", "Spigot"],
+        ["https://avatars.githubusercontent.com/u/7608950?s=200&v=4", "PaperMC"],
+        ["DecompMe.png", "decomp.me"],
+        ["https://appwrite.io/assets/logomark/logo.png", "Appwrite"],
+        ["https://raw.githubusercontent.com/emilk/egui/refs/heads/main/web_demo/favicon.ico", "egui"]
+    ]
+</script>
+
+<div class="flex flex-col items-center">
+    <h1 class="text-8xl pt-10 text-center">hai, i'm</h1>
+    <img src="name_plate.png" alt="nibbles" class="h-[10em]" style="image-rendering: pixelated">
+    <p class="text-4xl pt-5 text-center">i'm a programmer/game developer/music composer who's been making stuff for over 5 years!!</p>
+    <p class="text-3xl pt-5 text-center">here are some languages i know sorted by how much i like them!</p>
+
+    <div class="flex flex-row items-center flex-wrap justify-center">
+        {#each langs as lang}
+        <div>
+            <img src={lang[0]} class="w-[6em] h-[6em] p-2 absolute z-10" alt={lang[1]}/>
+            <img src={lang[0]} class="w-[6em] h-[6em] p-2 blur-sm opacity-75" alt={lang[1]}/>
+            <p class="text-center text-sm">{lang[1]}</p>
+        </div>
+        
+        {/each}
+    </div>
+
+    <p class="text-3xl pt-5 text-center">and the same thing but with tools/frameworks/software in no particular order (i love them all)!</p>
+
+    <div class="flex flex-row items-center flex-wrap justify-center">
+        {#each tools as tool}
+        <div>
+            <img src={tool[0]} class="w-[6em] h-[6em] p-2 absolute z-10 object-contain" alt={tool[1]}/>
+            <img src={tool[0]} class="w-[6em] h-[6em] p-2 blur-sm object-contain opacity-75" alt={tool[1]}/>
+            <p class="text-center text-sm">{tool[1]}</p>
+        </div>
+        
+        {/each}
+    </div>
+</div>
