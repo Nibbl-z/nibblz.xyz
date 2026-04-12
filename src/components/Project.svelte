375250ae232e919585243964f4bf721ac9b5ac8f
Nibbl-z <thisisnotnibblz123@gmail.com>
2026-04-12 00:30:08 -0600

    feat(projectspage): :sparkles: add fade in animation when scrolling through projects
    its cool !

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -1,30 +1,45 @@
 <script lang="ts">
-    export let name: string
-    export let image: string
-    export let description: string
-    export let footer: string | undefined = undefined
-    export let link: string
+    import { fade } from 'svelte/transition';
+    import { onMount } from "svelte";
+
+    let {name, image, description, footer = undefined, link}: {name: string, image: string, description: string, footer: string | undefined, link: string} = $props()
+
+    let inView = $state(false)
+    let element: Element;
+
+    onMount(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                inView = entry.isIntersecting
+            });
+        });
+
+        observer.observe(element);
+    })
 </script>
 
-<div class="w-full border-8 border-[#0821FF] backdrop-blur-lg mb-10">
-    <div class="w-full h-full absolute outline-8 outline-[#0821FF] blur-lg -z-10"></div>
-    <div class="p-5">
-        <h1 class="text-4xl lg:text-6xl">{name}</h1>
-        <div class="flex xl:flex-row flex-col pt-5">
-            <p class="text-2xl lg:text-3xl xl:w-[50%] p-2">{description}</p>
-            {#if !image.includes("youtube.com")}
-                <img src={image} alt={name} class="xl:w-[50%] max-h-130 object-contain p-2 rounded-2xl"/>
-            {:else}
-                <iframe class="xl:w-[50%] aspect-video object-contain p-2 rounded-2xl" src={image} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+<div bind:this={element}>
+    <div class="w-full border-8 border-[#0821FF] backdrop-blur-lg mb-10" class:notvisible={!inView} class:fade>
+        <div class="w-full h-full absolute outline-8 outline-[#0821FF] blur-lg -z-10"></div>
+        <div class="p-5">
+            <h1 class="text-4xl lg:text-6xl">{name}</h1>
+            <div class="flex xl:flex-row flex-col pt-5">
+                <p class="text-2xl lg:text-3xl xl:w-[50%] p-2">{description}</p>
+                {#if !image.includes("youtube.com")}
+                    <img src={image} alt={name} class="xl:w-[50%] max-h-130 object-contain p-2 rounded-2xl"/>
+                {:else}
+                    <iframe class="xl:w-[50%] aspect-video object-contain p-2 rounded-2xl" src={image} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                {/if}
+                
+            </div>
+            {#if footer}
+                <p class="text-xl text-center text-gray-500!">{footer}</p>
             {/if}
-            
+            <button class="w-full pt-5 border-8 border-[#7BE7FF] hover:border-[#0821FF]">
+                <a class="text-center text-6xl" href={link} target="_blank">Check it out!</a>
+            </button>
         </div>
-        {#if footer}
-            <p class="text-xl text-center text-gray-500!">{footer}</p>
-        {/if}
-        <button class="w-full pt-5 border-8 border-[#7BE7FF] hover:border-[#0821FF]">
-            <a class="text-center text-6xl" href={link} target="_blank">Check it out!</a>
-        </button>
+        
     </div>
-    
 </div>
